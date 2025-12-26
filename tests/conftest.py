27deafa361407fd9a8986d5a923148c5b1f902c8
Vinay vishwakarma <vinaymac@Vinays-MacBook-Pro.local>
2025-12-26 18:25:12 +0530

    headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,35 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-# 1. यह फंक्शन Pytest को बताता है कि '--browser_name' एक मान्य कमांड है
 def pytest_addoption(parser):
-    parser.addoption(
-        "--browser_name", action="store", default="chrome", help="Choose browser: chrome or firefox"
-    )
+    parser.addoption("--browser_name", action="store", default="chrome")
 
 
 @pytest.fixture(scope="function")
 def setup(request):
-    # 2. यहाँ हम कमांड लाइन से पास की गई वैल्यू को पढ़ते हैं
     browser = request.config.getoption("browser_name")
 
     if browser == "chrome":
         chrome_options = Options()
+        # --- ये 3 लाइनें Headless के लिए ज़रूरी हैं ---
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        # ------------------------------------------
+
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.notifications": 2
         })
-        # Jenkins (Linux/Mac Server) के लिए अक्सर headless मोड की ज़रूरत होती है
-        # chrome_options.add_argument("--headless")
         driver = webdriver.Chrome(options=chrome_options)
 
     elif browser == "firefox":
-        driver = webdriver.Firefox()
-
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        # Firefox के लिए भी headless कर सकते हैं
+        from selenium.webdriver.firefox.options import Options as FFOptions
+        ff_options = FFOptions()
+        ff_options.add_argument("--headless")
+        driver = webdriver.Firefox(options=ff_options)
 
     driver.maximize_window()
     yield driver
